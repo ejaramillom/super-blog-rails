f77cdeab74a92a9b6c720038ab3eafeb70b2cb20
ejaramillom <ejaramilo@hotmail.com>
2020-04-22 08:58:41 -0500

    added traslation to error messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,5 @@
-postUno = Post.create(title: "Sinopsis: Colors Out Of Space", author: "Emmanuel Jaramillo Muñoz",
-  content: "Color Out of Space es una película de ciencia ficción estadounidense de terror
-  cósmico de 2019 dirigida y coescrita por Richard Stanley, basada en el cuento
-  \"The Color Out of Space\" de H. P. Lovecraft. Está protagonizada por Nicolas Cage, Joely
-  Richardson, Madeleine Arthur, Q'orianka Kilcher y Tommy Chong. Este es el primer
-  largometraje de Stanley dirigido desde su despido de The Island of Dr. Moreau (1996).
-  Según Stanley, es la primera película de una trilogía de adaptaciones de Lovecraft,
-  que espera seguir con una adaptación de \"The Dunwich Horror\".")
+postUno = Post.create(title: "Sinopsis: Colors Out Of Space", author: "Emmanuel Jaramillo Muñoz", content: "Color Out of Space es una película de ciencia ficción estadounidense de terror cósmico de 2019 dirigida y coescrita por Richard Stanley, basada en el cuento \"The Color Out of Space\" de H. P. Lovecraft. Está protagonizada por Nicolas Cage, Joely Richardson, Madeleine Arthur, Q'orianka Kilcher y Tommy Chong. Este es el primer largometraje de Stanley dirigido desde su despido de The Island of Dr. Moreau (1996). Según Stanley, es la primera película de una trilogía de adaptaciones de Lovecraft, que espera seguir con una adaptación de \"The Dunwich Horror\".")
 
-postDos = Post.create(title: "Llega la PS5", author: "Mark Cerny",
-  content: "La PlayStation 5 (PS5) es una próxima consola de videojuegos para el hogar desarrollada
-  por Sony Interactive Entertainment. Anunciado como el sucesor de la PlayStation 4 en 2019, su
-  lanzamiento está programado para finales de 2020. En un artículo de Wired en abril de 2019,
-  el arquitecto principal de Sony, Mark Cerny, reveló información sobre el sucesor de la
-  PlayStation 4. La nueva consola tiene una unidad de estado sólido especializada,
-  una GPU AMD personalizada capaz de ray tracing, compatibilidad con la mayoría de juegos de
-  las PlayStation 4 y juegos de PlayStation VR, y soporte para juegos digitales y basados
-  en disco.")
+postDos = Post.create(title: "Llega la PS5", author: "Mark Cerny", content: "La PlayStation 5 (PS5) es una próxima consola de videojuegos para el hogar desarrollada por Sony Interactive Entertainment. Anunciado como el sucesor de la PlayStation 4 en 2019, su lanzamiento está programado para finales de 2020. En un artículo de Wired en abril de 2019, el arquitecto principal de Sony, Mark Cerny, reveló información sobre el sucesor de la PlayStation 4. La nueva consola tiene una unidad de estado sólido especializada, una GPU AMD personalizada capaz de ray tracing, compatibilidad con la mayoría de juegos de las PlayStation 4 y juegos de PlayStation VR, y soporte para juegos digitales y basados en disco.")
 
-postTres = Post.create(title: "Avances en computadores cuánticos", author: "Paul Benioff",
-  content: "Quantum computing is the use of quantum-mechanical phenomena such as superposition
-  and entanglement to perform computation. Computers that perform quantum computation are known
-  as a quantum computers. Quantum computers are believed to be able to solve certain
-  computational problems, such as integer factorization (which underlies RSA encryption),
-  significantly faster than classical computers. The study of quantum computing is a subfield
-  of quantum information science.")
+postTres = Post.create(title: "Avances en computadores cuánticos", author: "Paul Benioff", content: "Quantum computing is the use of quantum-mechanical phenomena such as superposition and entanglement to perform computation. Computers that perform quantum computation are known as a quantum computers. Quantum computers are believed to be able to solve certain computational problems, such as integer factorization (which underlies RSA encryption), significantly faster than classical computers. The study of quantum computing is a subfield of quantum information science.")
